f468d316e1aa08a73a6a3e73b84270f62999d246
Sajal Sood <sajal.sood@gmail.com>
2020-02-26 17:00:34 -0500

    External Urls

--- csye6225/Program.cs
+++ csye6225/Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -9,18 +10,18 @@ using Microsoft.Extensions.Logging;
 
 namespace csye6225
 {
-    public class Program
+     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+           WebHost.CreateDefaultBuilder(args)
+            .UseKestrel()
+            .UseUrls("http://*:5002;http://localhost:5001;http://localhost:5000")
+            .UseStartup<Startup>();
     }
+
 }
