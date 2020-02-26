981d859bab121d9f305213a1d413f8fd4dd692c9
Sajal Sood <sajal.sood@gmail.com>
2020-02-25 21:18:30 -0500

    updated .gitgnore

--- csye6225/Models/dbContext.cs
+++ csye6225/Models/dbContext.cs
@@ -23,8 +23,8 @@ namespace csye6225.Models
                 var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
                 var configuration = new ConfigurationBuilder()
                     .SetBasePath(AppContext.BaseDirectory)
-                    .AddJsonFile($"appsettings.json", optional: true, reloadOnChange: true)
-                    .AddJsonFile($"appsettings.{env}.json", optional: true, reloadOnChange: true)
+                    .AddJsonFile($"appsettings.json")
+                    .AddJsonFile($"appsettings.{env}.json")
                     .AddEnvironmentVariables()
                     .Build();
 
