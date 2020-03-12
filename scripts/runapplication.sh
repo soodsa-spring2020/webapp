337f1e6b894a0c3ce293005332fbcfb243bc6434
Sajal Sood <sajal.sood@gmail.com>
2020-03-12 18:29:59 -0400

    deploy

--- scripts/runapplication.sh
+++ scripts/runapplication.sh
@@ -2,18 +2,20 @@
 
 cd /home/ubuntu/webapp/
 dotnet publish -c Release
-touch /etc/systemd/system/dotnetcore.service
-echo "[Unit]" > /etc/systemd/system/dotnetcore.service
-echo "Description=Dot-Net-Core service" > /etc/systemd/system/dotnetcore.service
-echo "After=network.target" >> /etc/systemd/system/dotnetcore.service
-echo "[Service]" >> /etc/systemd/system/dotnetcore.service
-echo "ExecStart=/usr/bin/dotnet /home/ubuntu/webapp/csye6225/bin/Release/netcoreapp3.0/csye6225.dll" >> /etc/systemd/system/dotnetcore.service
-echo "Restart=on-failure" >> /etc/systemd/system/dotnetcore.service
-echo "WorkingDirectory=/home/ubuntu/webapp/" >> /etc/systemd/system/dotnetcore.service
-echo "User=ubuntu" >> /etc/systemd/system/dotnetcore.service
-echo "Group=ubuntu" >> /etc/systemd/system/dotnetcore.service
-echo "[Install]" >> /etc/systemd/system/dotnetcore.service
-echo "WantedBy=multi-user.target" >> /etc/systemd/system/dotnetcore.service
+if [ ! -f /etc/systemd/system/dotnetcore.service ]; then
+    touch /etc/systemd/system/dotnetcore.service
+    echo "[Unit]" > /etc/systemd/system/dotnetcore.service
+    echo "Description=Dot-Net-Core service" > /etc/systemd/system/dotnetcore.service
+    echo "After=network.target" >> /etc/systemd/system/dotnetcore.service
+    echo "[Service]" >> /etc/systemd/system/dotnetcore.service
+    echo "ExecStart=/usr/bin/dotnet /home/ubuntu/webapp/csye6225/bin/Release/netcoreapp3.0/csye6225.dll" >> /etc/systemd/system/dotnetcore.service
+    echo "Restart=on-failure" >> /etc/systemd/system/dotnetcore.service
+    echo "WorkingDirectory=/home/ubuntu/webapp/" >> /etc/systemd/system/dotnetcore.service
+    echo "User=ubuntu" >> /etc/systemd/system/dotnetcore.service
+    echo "Group=ubuntu" >> /etc/systemd/system/dotnetcore.service
+    echo "[Install]" >> /etc/systemd/system/dotnetcore.service
+    echo "WantedBy=multi-user.target" >> /etc/systemd/system/dotnetcore.service
+fi
 systemctl start dotnetcore
 systemctl enable dotnetcore
 
