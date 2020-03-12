b3123eeede318e5eacb017aa092e004b74d4f5e4
Sajal Sood <sajal.sood@gmail.com>
2020-03-12 17:59:58 -0400

    deploy fix

--- csye6225/Program.cs
+++ csye6225/Program.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
@@ -11,23 +12,24 @@ namespace csye6225
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
-
-            // var host = CreateWebHostBuilder(args).Build();
-
-            // using (var scope = host.Services.CreateScope())
-            // {
-            //     var db = scope.ServiceProvider.GetService<DbContext>();
-            //     db.Database.Migrate();
-            // }
-
-            // host.Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-           WebHost.CreateDefaultBuilder(args)
-            .UseKestrel()
-            .UseUrls("http://*:5002;http://localhost:5001;http://localhost:5000")
-            .UseStartup<Startup>();
+        // public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        //    WebHost.CreateDefaultBuilder(args)
+        //     .UseKestrel()
+        //     .UseUrls("http://*:5002;http://localhost:5001;http://localhost:5000")
+        //     .UseStartup<Startup>();
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
+                .UseKestrel(options =>
+                {
+                    options.Listen(IPAddress.Any, 80);
+                    options.Listen(IPAddress.Any, 5002);
+                })
+                .UseStartup<Startup>();
+        }
     }
 
 }
