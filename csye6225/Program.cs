4e54888fe4f3f7c10eb61a546003261f08abbf55
Sajal Sood <sajal.sood@gmail.com>
2020-03-13 00:41:51 -0400

    deploy fix

--- csye6225/Program.cs
+++ csye6225/Program.cs
@@ -16,29 +16,29 @@ namespace csye6225
             CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-           WebHost.CreateDefaultBuilder(args)
-            .UseKestrel()
-            .UseUrls("http://*:5002;http://localhost:5001;http://localhost:5000")
-            .UseStartup<Startup>();
+        // public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        //    WebHost.CreateDefaultBuilder(args)
+        //     .UseKestrel()
+        //     .UseUrls("http://*:5002;http://localhost:5001;http://localhost:5000")
+        //     .UseStartup<Startup>();
 
-        // public static IWebHostBuilder CreateWebHostBuilder(string[] args)
-        // {
-        //     return WebHost.CreateDefaultBuilder(args)
-        //         .UseKestrel(options =>
-        //         {
-        //             options.Limits.MaxConcurrentConnections = 100;
-        //             options.Limits.MaxConcurrentUpgradedConnections = 100;
-        //             options.Limits.MaxRequestBodySize = 10 * 1024;
-        //             options.Limits.MinRequestBodyDataRate =
-        //             new MinDataRate(bytesPerSecond: 100, gracePeriod: TimeSpan.FromSeconds(10));
-        //             options.Limits.MinResponseDataRate =
-        //             new MinDataRate(bytesPerSecond: 100, gracePeriod: TimeSpan.FromSeconds(10));
-        //             options.Listen(IPAddress.Any, 80);
-        //             options.Listen(IPAddress.Any, 5002);
-        //         })
-        //         .UseStartup<Startup>();
-        // }
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
+                .UseKestrel(options =>
+                {
+                    options.Limits.MaxConcurrentConnections = 100;
+                    options.Limits.MaxConcurrentUpgradedConnections = 100;
+                    options.Limits.MaxRequestBodySize = 10 * 1024;
+                    options.Limits.MinRequestBodyDataRate =
+                    new MinDataRate(bytesPerSecond: 100, gracePeriod: TimeSpan.FromSeconds(10));
+                    options.Limits.MinResponseDataRate =
+                    new MinDataRate(bytesPerSecond: 100, gracePeriod: TimeSpan.FromSeconds(10));
+                    options.Listen(IPAddress.Any, 80);
+                    options.Listen(IPAddress.Any, 5002);
+                })
+                .UseStartup<Startup>();
+        }
     }
 
 }
--- scripts/stopapplication.sh
+++ scripts/stopapplication.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-if (systemctl -q is-active dotnetcore.service)
-    then
-    systemctl stop dotnetcore.service
+if [ systemctl -q is-active dotnetcore.service ]; then
+    systemctl stop dotnetcore
 fi
